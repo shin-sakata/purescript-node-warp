ba30d433fb6f05434950d772775fc8bd012d3bae
chemirea <shintaro.sakata.tokyo@gmail.com>
2020-06-27 19:03:12 +0900

    remove comment for debug

--- src/Network/Node/Warp.purs
+++ src/Network/Node/Warp.purs
@@ -19,38 +19,6 @@ import URI.Extra.QueryPairs as QE
 import URI.Query as Q
 import Network.Wai as Wai
 
-{-|
-example
-Request {
-    requestMethod = "GET",
-    httpVersion = HTTP/1.1, 
-    rawPathInfo = "/one/two", 
-    rawQueryString = "?query=value&query2=value2", 
-    requestHeaders = [
-        ("Host","localhost:8081"),
-        ("Connection","keep-alive"),
-        ("Cache-Control","max-age=0"),
-        ("Upgrade-Insecure-Requests","1"),
-        ("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"),
-        ("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"),
-        ("Sec-Fetch-Site","none")
-        ,("Sec-Fetch-Mode","navigate"),
-        ("Sec-Fetch-User","?1"),
-        ("Sec-Fetch-Dest","document"),
-        ("Accept-Encoding","gzip, deflate, br"),
-        ("Accept-Language","ja,en-US;q=0.9,en;q=0.8"),
-    ],
-    remoteHost = 127.0.0.1:49714,
-    pathInfo = ["one","two"],
-    queryString = [("query",Just "value"),("query2",Just "value2")],
-    requestBody = <IO ByteString>,
-    vault = <Vault>,
-    requestBodyLength = KnownLength 0,
-    requestHeaderHost = Just "localhost:8081",
-    requestHeaderRange = Nothing
-}
--}
-
 toMethod :: HTTP.Request -> H.Method
 toMethod req = case H.parseMethod $ HTTP.requestMethod req of
     Right method -> method
